6ed55758440698a6499bbfddc8a0bcd3f26716a4
alvesmariadefatima <mnunesalves334@gmail.com>
2024-08-22 15:49:56 -0300

    Configuration files local of the blockchain HardHat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,21 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import dotenv from 'dotenv';
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    avartest: {
+      url: process.env.AVARTEST_URL || "",
+      chainId: Number(process.env.AVARTEST_CHAINID) || 0,
+      accounts: process.env.SECRET ? [process.env.SECRET] : [],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY || "",
+  }
 };
 
 export default config;
